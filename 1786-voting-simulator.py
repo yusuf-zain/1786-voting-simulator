e203f3b9c1e04ac99e8ca9a6a3117384f4097c38
Yusuf Ansari <68573429+yusuf-zain@users.noreply.github.com>
2020-08-05 00:18:47 -0400

    Add files via upload

---
+++ 1786-voting-simulator.py
@@ -0,0 +1,32 @@
+import sys
+print("""""")
+print("""Welcome to Voting Simulator 1786.
+Before we can decide whether you could vote,
+we will have to ask you some questions.
+Please answer in a yes/no format (besides age).
+""")
+closing = """Thank you for playing!"""
+landowner = (input("Do you own any land?").lower())
+if landowner == "no":
+    print("""   You can't vote""")
+    print(closing)
+    sys.exit()
+Skin = (input("Are you African American").lower())
+if Skin == "yes":
+    print(" You can't vote")
+    print(closing)
+    sys.exit()
+gender = (input("Are you female").lower())
+if gender == "yes":
+    print(" You can't vote")
+    print(closing)
+    sys.exit()
+age = (int(input("How old are you")))
+if age < 21:
+    print(" You can't vote")
+    print(closing)
+    sys.exit()
+if age >= 21:
+    print(" Congratulations! You are a part of the 20% of the population who could vote at that time!")
+    print(closing)
+sys.exit()
