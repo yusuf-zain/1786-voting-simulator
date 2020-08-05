dd0d00e656a0e0649e696d5e5d4fe107ecff8790
Yusuf Ansari <68573429+yusuf-zain@users.noreply.github.com>
2020-08-05 00:35:34 -0400

    Update 1786-voting-simulator.py

--- 1786-voting-simulator.py
+++ 1786-voting-simulator.py
@@ -6,22 +6,22 @@ we will have to ask you some questions.
 Please answer in a yes/no format (besides age).
 """)
 closing = """Thank you for playing!"""
-landowner = (input("Do you own any land?").lower())
+landowner = (input("Do you own any land? ").lower())
 if landowner == "no":
     print("""   You can't vote""")
     print(closing)
     sys.exit()
-Skin = (input("Are you African American").lower())
+Skin = (input("Are you African American? ").lower())
 if Skin == "yes":
     print(" You can't vote")
     print(closing)
     sys.exit()
-gender = (input("Are you female").lower())
+gender = (input("Are you female? ").lower())
 if gender == "yes":
     print(" You can't vote")
     print(closing)
     sys.exit()
-age = (int(input("How old are you")))
+age = (int(input("How old are you? ")))
 if age < 21:
     print(" You can't vote")
     print(closing)
